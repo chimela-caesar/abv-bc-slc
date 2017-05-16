a3f4756b4bb3ffea6bc342ca62b3bd4132104a79
chimela-caesar <chimela.caesar@gmail.com>
2017-05-16 10:37:34 +0100

    Test for non-integer value of n passed

--- app/primes.js
+++ app/primes.js
@@ -5,6 +5,8 @@ var exports = module.exports = {};
 /* This function finds all prime numbers up to n using 2n log log n + cn + O(n / log n) arithmetic operations, where c is aproximately 1.14 */
 exports.getPrimes = function(n)
 {
+  if (!Number.isInteger(n)) return false;
+	
   const primes = [];
   
   return primes;
