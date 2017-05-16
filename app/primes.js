b2fb21aa8b71205d3e4c547e0c19ce2e50d0e85d
chimela-caesar <chimela.caesar@gmail.com>
2017-05-16 11:36:24 +0100

    Add all tests for the get primes functionality

--- app/primes.js
+++ app/primes.js
@@ -5,9 +5,21 @@ var exports = module.exports = {};
 /* This function finds all prime numbers up to n using 2n log log n + cn + O(n / log n) arithmetic operations, where c is aproximately 1.14 */
 exports.getPrimes = function(n)
 {
-  if (!Number.isInteger(n)) return false;
-	
+  if (!Number.isInteger(n) || Math.sign(n) === -1 || n === 0) return false;
+
   const primes = [];
+  const isComposite = Array(n + 1).fill(false);
   
+  for(let i = 2; i <= n; i++) {
+    
+    if(!isComposite[i]) {
+      primes.push(i);
+ 
+      for(let j = i * i; j >= 0 && j <= n; j += i) {
+        isComposite[j] = true;
+      }
+    }
+  }
+ 
   return primes;
 };
--- spec/primesSpec.js
+++ spec/primesSpec.js
@@ -4,7 +4,39 @@ describe("Get Primes Functionality",function(){
     expect(app.getPrimes("a string")).toEqual(false);
   });
   
-  it("should return false for non-integer value of n", function() {
-    expect(app.getPrimes("a string")).toEqual(false);
+  it("should return false for negative value of n", function() {
+    expect(app.getPrimes(-2)).toEqual(false);
+  });
+  
+  it("should return false for a zero value of n", function() {
+    expect(app.getPrimes(0)).toEqual(false);
+  });
+  
+  it("should return an empty array if no primes are obtainable", function() {
+    expect(app.getPrimes(1)).toEqual([]);
+  });
+  
+  it("should return an array for valid values of n", function() {
+    expect(app.getPrimes(25) instanceof Array).toBeTruthy();
+  });
+  
+  it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23 ] for n = 25", function() {
+    expect(app.getPrimes(25)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23 ]);
+  });
+  
+  it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ] for n = 30", function() {
+    expect(app.getPrimes(30)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
+  });
+  
+  it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ] for n = 35", function() {
+    expect(app.getPrimes(35)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ]);
+  });
+  
+  it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ] for n = 40", function() {
+    expect(app.getPrimes(40)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ]);
+  });
+  
+  it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ] for n = 50", function() {
+    expect(app.getPrimes(50)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]);
   });
 });
