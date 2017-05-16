a8f15bab594497868e370c8d0258746edfc4b2bb
chimela-caesar <chimela.caesar@gmail.com>
2017-05-16 10:40:17 +0100

    Add test for negative value of n

--- spec/primesSpec.js
+++ spec/primesSpec.js
@@ -3,4 +3,8 @@ describe("Get Primes Functionality",function(){
   it("should return false for non-integer value of n", function() {
     expect(app.getPrimes("a string")).toEqual(false);
   });
+  
+  it("should return false for non-integer value of n", function() {
+    expect(app.getPrimes("a string")).toEqual(false);
+  });
 });
